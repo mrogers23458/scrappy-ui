0675d220f48732dec6fa3d5e5d62c74c60f8a7e5
unknown <mrogers23458@gmail.com>
2025-02-10 21:21:41 -0800

    updated vite config for npm lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import dts from "vite-plugin-dts";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-})
+  plugins: [react(), dts(), tailwindcss()],
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      name: "ScrappyUI",
+      fileName: (format) => `scrappy-ui.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
